d291042cf5a29b9aa325060fa3bb7e56b6d43eb7
JavierCollipal <jcollipal1212@gmail.com>
2023-01-29 12:52:16 -0300

    graph mode and config with env works fine

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { CatsModule } from './cats/cats.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(),MongooseModule.forRoot(process.env.MONGO_URL, { useNewUrlParser: true }), GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  }), CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
